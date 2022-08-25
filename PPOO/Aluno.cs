b96a74c3ea9907a9794f3e126f517ed62b4b35dc
Danivictorsilva5by5 <daniel.silva@simberg.com.br>
2022-08-25 09:19:14 -0300

    Heranca de classes

---
+++ PPOO/Aluno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPOO
+{
+    internal class Aluno : Pessoa
+    {
+
+        protected String Registro;
+        protected DateTime DataMtla;
+
+        public Aluno() { }
+
+        public void setRegistro()
+        {
+            Console.Write("Digite o numero de Registro: ");
+            this.Registro = Console.ReadLine();
+        }
+        public String getRegistro()
+        {
+            return Registro;
+        }
+        public void setDataMtla()
+        {
+            Console.Write("Digite a Data de Matricula: ");
+            this.DataMtla = DateTime.Parse(Console.ReadLine());
+        }
+        public String getDataMtla()
+        {
+            return DataMtla.ToShortDateString();
+        }
+        public override string ToString()
+        {
+            return String.Format("{{'Nome': {0},\n" +
+                "'Data de Nascimento': {1},\n" +
+                "'CPF': {2},\n" +
+                "'Registro': {3},\n" +
+                "'Data de Registro': {4}}}",
+                getNome(), getDataNcto(), getCPF(), getRegistro(), getDataNcto());
+        }
+        public void CadastrarAluno()
+        {
+            base.CadastrarPessoa();
+            setRegistro();
+            setDataMtla();
+        }
+    }
+}
---
+++ PPOO/Funcionario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPOO
+{
+    internal class Funcionario : Pessoa
+    {
+        protected String NPIS;
+        protected String Setor;
+        protected float Salario;
+
+        public Funcionario() { }
+
+        public void setNPIS()
+        {
+            Console.Write("Digite o Numero do PIS: ");
+            this.NPIS = Console.ReadLine();
+        }
+        public String getNPIS()
+        {
+            return NPIS;
+        }
+        public void setSetor()
+        {
+            Console.Write("Digite o Setor: ");
+            this.Setor = Console.ReadLine();
+        }
+        public String getSetor()
+        {
+            return Setor;
+        }
+        public void setSalario()
+        {
+            Console.Write("Digite o montante de Salario: ");
+            this.Salario = float.Parse(Console.ReadLine());
+        }
+        public float getSalario()
+        {
+            return Salario;
+        }
+        public override string ToString()
+        {
+            return String.Format("{{'Nome': {0},\n" +
+                "'Data de Nascimento': {1},\n" +
+                "'CPF': {2},\n" +
+                "'NPIS': {3},\n" +
+                "'Setor': {4},\n" +
+                "'Salario': {5}}}",
+                getNome(), getDataNcto(), getCPF(), getNPIS(),getSetor(),getSalario());
+        }
+        public void CadastrarFuncionario()
+        {
+            base.CadastrarPessoa();
+            setNPIS();
+            setSetor();
+            setSalario();
+        }
+    }
+}
